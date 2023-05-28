95fd0da837f2aaa1376537570b01f3b562c8b50b
Your Name <you@example.com>
2023-05-28 20:42:27 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { solToLamports, lamportsToSol } from "gamba";
+import { Gamba, useGamba } from "gamba/react";
+import { GambaModalButton, GambaUi } from "gamba-react-ui";
+
+function Game() {
+  const gamba = useGamba();
+  return (
+    <div className="game-container">
+      <div className="button-container">
+        <GambaModalButton />
+      </div>
+      <div className="button-container">
+        <button onClick={() => gamba.play([0, 2], solToLamports(0.01))}>
+          Play
+        </button>
+      </div>
+      <div className="button-container">
+        <button onClick={() => gamba.withdraw()}>
+          {lamportsToSol(gamba.user?.balance)} Claim
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Gamba
+      creator="5Wu7wbvCXYHrER7tnTyV3SzzMyoDaJoyRNFQVms3kvvg"
+      connection={{
+        endpoint:
+          "https://rpc.helius.xyz/?api-key=d1b8ca10-0ab9-4f59-9568-18a686943a7f",
+      }}
+    >
+      <GambaUi>
+        <Game />
+      </GambaUi>
+    </Gamba>
   );
 }
 
